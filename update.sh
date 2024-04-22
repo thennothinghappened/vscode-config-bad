956d05deba9078217241ba24e9233fd13ddb12a7
thennothinghappened <35243139+thennothinghappened@users.noreply.github.com>
2024-04-23 08:30:18 +1000

    Add update script

---
+++ update.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# 
+# (MacOS) Update `settings.json` and `keybindings.json` from the VSCode copy.
+
+cp "/Users/orca/Library/Application Support/Code/User/keybindings.json" "./keybindings.json"
+cp "/Users/orca/Library/Application Support/Code/User/settings.json" "./settings.json"
+
